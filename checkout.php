f88dc3e515e79b3fc4af3e40f87fd5e5bf9a9e10
StevenJ <s.jespers@telenet.be>
2016-12-07 16:00:37 +0100

    bezig met exception van date

--- checkout.php
+++ checkout.php
@@ -4,7 +4,9 @@ session_start();
 require_once 'bootstrap.php';
 
 use Steven\Eindtest\Business\OrderService;
+use Steven\Eindtest\Exceptions\DateOutOfBoundsException;
 $isLoggedIn = false;
+$error = null;
 //print "blabla";
 
 if (isset($_SESSION["email"])) {
@@ -13,14 +15,18 @@ if (isset($_SESSION["email"])) {
     if (isset($_SESSION["cart"])) {
         $cart = unserialize($_SESSION["cart"]);
         if (isset($_POST["confirmSubmit"])) {
+            try{
             $orderSvc = new OrderService();
             $orderSvc->confirm($cart, $_SESSION["email"], $_POST["date"]);
             unset($_SESSION["cart"]);
             header("location: orders.php");
             exit(0);
+            }catch(DateOutOfBoundsException $ex){
+                $error = "Datum moet na vandaag zijn en maximum drie dagen in de toekomst.";
+            }
         }
     }
-    $view = $twig->render("checkout.twig", array("cart" => $cart, "isLoggedIn" => $isLoggedIn));
+    $view = $twig->render("checkout.twig", array("cart" => $cart, "isLoggedIn" => $isLoggedIn, "error" => $error));
 
     print($view);
 } else {
--- src/Steven/Eindtest/Business/OrderService.php
+++ src/Steven/Eindtest/Business/OrderService.php
@@ -4,9 +4,15 @@
 
 namespace Steven\Eindtest\Business;
 use Steven\Eindtest\Data\OrderDAO;
+use Steven\Eindtest\Exceptions\DateOutOfBoundsException;
 
 class OrderService {
     public function confirm($cart, $email, $date){
+        $dateNow = date_create(date('Y-m-d'));
+        $dateLate = date_add($dateNow, date_interval_create_from_date_string("3days"));
+        if($date <= $dateNow || $date > $dateLate){
+            throw new DateOutOfBoundsException();
+        }
         $cartlines = $cart->getCart();
         $orderDao = new OrderDAO();
         $userSvc = new UserService();
